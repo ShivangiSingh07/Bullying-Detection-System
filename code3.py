3a3933a98e136bf6aecccc3c86d35b05d927b5e9
Shivangi Singh <shivangisingh7@outlook.com>
2026-02-19 23:58:22 +0530

    Add decision-making and user input examples

---
+++ code3.py
@@ -0,0 +1,22 @@
+# code3_conditions.py
+# Learning decision-making with if/else
+# Asking for user input
+age = int(input("Enter your age: "))
+
+# Decision-making
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior.")
+
+# Boolean example
+is_student = input("Are you a student? (yes/no): ")
+
+if is_student.lower() == "yes":
+    print("Keep learning and growing!")
+else:
+    print("Lifelong learning is still important!")
