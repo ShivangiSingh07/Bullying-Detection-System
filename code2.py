573d5ceae104e98ed9e50a88e7d099144afbe36e
Shivangi Singh <shivangisingh7@outlook.com>
2026-02-19 23:52:34 +0530

    Add basic variable and input/output examples

---
+++ code2.py
@@ -0,0 +1,16 @@
+# day2_basics.py
+# Learning variables, input, and output
+
+# Storing values
+name = "Shivangi"
+age = 18
+is_student = True
+
+# Printing values
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+
+# Taking input
+fav_lang = input("Enter your favorite programming language: ")
+print("Cool! You like", fav_lang)
