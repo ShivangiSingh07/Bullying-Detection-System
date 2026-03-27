42467087c1893e566c53915b7f9c86bdbca93bdc
Shivangi Singh <shivangisingh7@outlook.com>
2026-03-27 21:25:05 +0530

    Delete code1.py

--- code1.py
+++
@@ -1,8 +0,0 @@
-# hello.py
-# My very first Python program!
-# Starting small but consistent—this marks Day 1 of my Python journey.
-
-print("Hello, Python!")
-print("This is Shivangi learning Python.")
-
-
